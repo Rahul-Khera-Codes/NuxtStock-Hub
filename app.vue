41d27272c181e3a878327eee96f0a1651b33559c
Rahul-Khera-Codes <rahul.excel2011@gmail.com>
2024-12-05 10:53:24 +0100

    progress update: pushing incremental changes

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="tw-min-h-screen tw-flex tw-flex-col tw-flex-1 tw-bg-gray-900">
+    <BasicDemo />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import BasicDemo from './demos/BasicDemo.vue'
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+const plugin = require('tailwindcss/plugin')
+export default {
+  mode: 'jit',
+  prefix: 'tw-',
+  content: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './app.vue',
+    './pages/**/*.vue',
+    './demos/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    // './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        18: '4.5rem',
+      },
+      minHeight: {
+        45: '11.25rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.text-shadow': {
+          textShadow: '0px 0px 3px rgb(0 0 0)',
+        },
+      })
+    }),
+  ],
+}
